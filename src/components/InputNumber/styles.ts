b8a42ed8ce7768544a599e7073c3f6b63806bf56
Priscilla <priscillaonil@gmail.com>
2023-11-09 13:55:51 -0300

    perf: improved accessibility

--- src/components/InputNumber/styles.ts
+++ src/components/InputNumber/styles.ts
@@ -18,6 +18,8 @@ export const InputNumberBtn = styled.button`
     background-color: ${theme.colors.neutral[400]};
     color: ${theme.colors.purple[500]};
     outline: none;
+    font-size: ${theme.font.sizes.l};
+    padding: 8px;
   `}
 `;
 
@@ -28,6 +30,8 @@ export const InputNumber = styled.input`
     width: 24px;
     text-align: center;
     outline: none;
+    padding: 8px;
+    font-size: ${theme.font.sizes.lm};
 
     &::-webkit-outer-spin-button {
       -webkit-appearance: none;
